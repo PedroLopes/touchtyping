66dc688b92d007af377ecfd634fd0d1626a184d8
Pedro Lopes <plopesresearch@gmail.com>
2017-06-18 01:18:43 +0200

    loaded exercises

--- touchtyping.py
+++ touchtyping.py
@@ -1,5 +1,6 @@
 from Getch import getch
 from sys import argv
+import argparse
 
 def insert_separator(words, char):
     words_spaces = []
@@ -8,12 +9,25 @@ def insert_separator(words, char):
         words_spaces.append(char)
     return words_spaces[:-1]
 
-words = ["hello", "world", "this", "is", "a", "touch", "typing", "tutorial"]
+parser = argparse.ArgumentParser(description='touch typing training in python')
+parser.add_argument('--strict', action="store_true", default=False)
+parser.add_argument('--exercise', action="store", dest="exercise", type=int)
+parser.add_argument('--study', action="store", dest="study")
+
+vals = parser.parse_args()
+
+if vals.exercise != None:
+    print("exercises/"+str(vals.exercise)+".txt")
+    file = open("exercises/"+str(vals.exercise)+".txt", "r") 
+    words = file.read().split(" ")[:-1]
+else:
+    words = ["hello", "world", "this", "is", "a", "touch", "typing", "tutorial"]
+
 words = insert_separator(words, " ")
 print(''.join(words))
 
 typos = 0
-threshold = 3
+max_typos = 3
 
 for word in words:
     char = ""
@@ -25,6 +39,8 @@ for word in words:
             i+=1
         else:
             typos+=1
-
-print('\nFinished with {} typos'.format(typos))
+print('\nFinished with {} typos (max is {})'.format(typos,max_typos))
+if vals.strict:
+    if typos > max_typos:
+        print('Too many typos. Try again')
 
