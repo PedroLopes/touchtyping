10d791b16c8fe2448933ce9d3a14d45f2c0035dc
Pedro Lopes <plopesresearch@gmail.com>
2017-06-18 02:24:18 +0200

    reads session from log

--- touchtyping.py
+++ touchtyping.py
@@ -11,17 +11,26 @@ def insert_separator(words, char):
     return words_spaces[:-1]
 
 parser = argparse.ArgumentParser(description='touch typing training in python (use ESC to quit)')
-parser.add_argument('--strict', action="store_true", default=False)
+parser.add_argument('--no-strict', action="store_true", dest="no_strict", default=False)
 parser.add_argument('--exercise', action="store", dest="exercise", type=int)
 parser.add_argument('--study', action="store", dest="study")
 
 vals = parser.parse_args()
 
+if vals.study != None:
+    with open("study_sessions.log") as f:
+        content = f.readlines()
+        content = [x.strip() for x in content]
+        for line in content:
+            if line.find(vals.study) != -1:
+                vals.exercise=int(line.split(":")[1])
+        print("Loaded username {} with exercise no. {}".format(vals.study,vals.exercise))
 if vals.exercise != None:
     file = open("exercises/"+str(vals.exercise)+".txt", "r") 
     words = file.read().split(" ")[:-1]
 else:
     words = ["hello", "world", "this", "is", "a", "touch", "typing", "tutorial"]
+
 
 words = insert_separator(words, " ")
 print(''.join(words))
@@ -42,7 +51,6 @@ for word in words:
         else:
             typos+=1
 print('\nFinished with {} typos (max is {})'.format(typos,max_typos))
-if vals.strict:
-    if typos > max_typos:
-        print('Too many typos. Try again')
+if typos > max_typos and vals.no_strict == True:
+    print('Too many typos. Try again')
 
